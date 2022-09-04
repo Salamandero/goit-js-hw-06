7e2f8b43401dec8227505045068ee25cc566991f
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2022-09-04 22:11:09 +0300

    fifnished 8th task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,2 +1,18 @@
 const form = document.querySelector('.login-form');
-// console.log(form);
+
+
+form.addEventListener('submit', (event) => {
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value.trim() === "" || password.value.trim() === "") {
+       return alert ('Всі поля повинні бути заповнені!');
+    }
+    event.preventDefault();
+    console.log(`Email: ${email.value}, password: ${password.value}`);
+    event.currentTarget.reset();
+});
+
+
+
+
