820f3833252e57363b261654782e93cb148121e3
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2022-09-11 16:18:54 +0300

    finished 9th task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const Ref = {
+  changeCol: document.querySelector('.change-color'),
+color: document.querySelector('.color'),
+body: document.querySelector('body'),
+}
+
+
+const changeBtnColor = () => {
+  Ref.body.style.background = getRandomHexColor();
+  Ref.color.textContent = getRandomHexColor();
+}
+Ref.changeCol.addEventListener('click', changeBtnColor);
